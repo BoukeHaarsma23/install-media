399536ed958c5692f8d52ccd0b186811dbee1c58
bouhaa <boukehaarsma23@gmail.com>
2023-10-03 09:28:04 +0200

    add stuff to dockerfile

--- chimeraos/airootfs/root/install.sh
+++ chimeraos/airootfs/root/install.sh
@@ -7,38 +7,6 @@ fi
 
 dmesg --console-level 1
 
-if [ ! -d /sys/firmware/efi/efivars ]; then
-    MSG="Legacy BIOS installs are not supported. You must boot the installer in UEFI mode.\n\nWould you like to restart the computer now?"
-    if (whiptail --yesno "${MSG}" 10 50); then
-        reboot
-    fi
-
-    exit 1
-fi
-
-
-#### Test conenction or ask the user for configuration ####
-
-# Waiting a bit because some wifi chips are slow to scan 5GHZ networks
-sleep 2
-
-TARGET="stable"
-while ! ( curl -Ls https://github.com | grep '<html' > /dev/null ); do
-    whiptail \
-     "No internet connection detected.\n\nPlease use the network configuration tool to activate a network, then select \"Quit\" to exit the tool and continue the installation." \
-     12 50 \
-     --yesno \
-     --yes-button "Configure" \
-     --no-button "Exit"
-
-    if [ $? -ne 0 ]; then
-         exit 1
-    fi
-
-    nmtui-connect
-done
-#######################################
-
 gamescope -- ./chimeraos-installer.x86_64
 
 bash -i
